ffa3b9cdc503eebefa06e44c5e720a2b06cbe188
yaiza0810 <yaiza.sanchez-bermudez@etu.univ-amu.fr>
2022-03-21 13:28:37 +0100

    ok

--- projet_solitaire/creation_scipt.py
+++ projet_solitaire/creation_scipt.py
@@ -5,18 +5,16 @@ def creer_plateau(nomFichier):
     with open("plateau/{}.txt".format(nomFichier), "r") as fichierPlateau:
         plateauTXT = fichierPlateau.readlines()
 
-    plateau = "bit plateau[] = {\t"
-    pions = "bit pions[] = {\t"
+    plateau = ""
+    pions = ""
 
     Nb_pion = 0
     placement_pion = []
 
     for j,ligne in enumerate(plateauTXT):
-        if j!=0:
-            plateau += "\t\t\t\t\t"
-            pions += "\t\t\t\t"
-
         for i,case in enumerate(ligne[:-1]):
+            plateau += "plateau[{}] = ".format(i+j*len(plateauTXT))
+            pions += "pions[{}] = ".format(i + j * len(plateauTXT))
             if case == "O":
                 plateau += " 1"
                 pions += " 1"
@@ -28,20 +26,11 @@ def creer_plateau(nomFichier):
             elif case == "X":
                 plateau += " 0"
                 pions += " 0"
+            plateau += ";\n"
+            pions += ";\n"
 
 
-            if i<len(ligne)-2:
-                plateau += ","
-                pions += ","
-
-        if j<len(plateauTXT)-1:
-            plateau += ",\n"
-            pions += ",\n"
-
-    plateau += "};\n\n"
-    pions += "};\n\n"
-
-    dim = "int Imax = {};\nint Jmax = {};\nint Nb_pion = {};\n\n".format(len(plateauTXT[0])-1,len(plateauTXT),Nb_pion)
+    dim = "int Imax = {};\nint Jmax = {};\nint Nb_pion = {};\nplateau[{}];\npions[{}];\n".format(len(plateauTXT[0])-1,len(plateauTXT),Nb_pion,(len(plateauTXT[0])-1)*len(plateauTXT),(len(plateauTXT[0])-1)*len(plateauTXT))
     return dim, plateau, pions, placement_pion
 
 
@@ -77,4 +66,4 @@ def creer_script(nomFichier):
     return script
 
 creer_script("plateau1")
-[](measure -> (((state_loup==state_mouton) || (state_chou== state_mouton))->(state_berger==state_mouton)))
+
